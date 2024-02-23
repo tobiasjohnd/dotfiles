e1312d8fd6cc861de794daff66c3bf9e70a2466f
Toby Dickenson <tbdickenson@googlemail.com>
2024-02-23 10:54:06 +0100

    Delete .xmonad directory

--- .xmonad/lib/MyConfig/DefaultApps.hs
+++
@@ -1,8 +0,0 @@
-module MyConfig.DefaultApps where
-
-terminal        = "alacritty"
-browser         = "brave-nightly"
-fileManager     = "alacritty -e nnn"
-fileManagerGui  = "nautilus"
-textEditor      = "alacritty -e nvim"
-appLauncher     = "rofi -modi run,drun -show drun"
