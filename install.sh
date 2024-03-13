d1ea5e80e2a5dbfa6912197921798ccad0aeefd6
Toby Dickenson <tbdickenson@googlemail.com>
2024-03-13 20:08:00 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+repo_dir="$HOME/.config-repo" 
+git_URL="git@github.com:TBDickenson/dotfiles.git"
+config() {
+  /usr/bin/git --git-dir="$repo_dir" --work-tree="$HOME" "$@"
+}
+
+git clone --bare $git_URL $repo_dir
+config checkout -f
+config config --local status.showUntrackedFiles no
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+
+sudo mkdir -p /opt/nvim
+sudo mv nvim.appimage /opt/nvim/nvim
