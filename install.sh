e69387055a30a19d5237815ffae495efd80b5564
Toby Dickenson <tbdickenson@googlemail.com>
2024-03-29 23:22:51 +0100

    adding in some loose ends

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+# TODO: see if i can automate ghcup installation + xmonad setup
+
 repo_dir="$HOME/.config-repo" 
 git_URL="git@github.com:TBDickenson/dotfiles.git"
 config() {
   /usr/bin/git --git-dir="$repo_dir" --work-tree="$HOME" "$@"
 }
 
-git clone --bare $git_URL $repo_dir
+git clone --recurse-submodules --bare $git_URL $repo_dir
 config checkout -f
 config config --local status.showUntrackedFiles no
 
